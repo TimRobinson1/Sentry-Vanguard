a4aa0dfb63483b66bc18669cd299650d3c6b0169
Tim Robinson <timmyrobby@gmail.com>
2020-05-06 06:34:18 +0100

    Add icon card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import qs from 'qs';
+import chunk from 'lodash/chunk';
 import { Chart } from 'react-charts'
-
-import logo from './logo.svg';
+import * as Icons from 'react-icons/fa';
 import './App.css';
 
-const data = [
+import infoData from './data/index.js';
+
+const chartData = [
   {
     label: 'Series 1',
     data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
@@ -19,6 +21,55 @@ const axes = [
   { primary: true, type: 'linear', position: 'bottom' },
   { type: 'linear', position: 'left' }
 ];
+
+const getStatusColor = (status) => {
+  switch (status) {
+    case 'good':
+      return '#85DE77';
+    case 'neutral':
+      return '#708090'
+    case 'okay':
+      return '#F3B600';
+    default:
+      return '#FF756D';
+  }
+}
+
+const InfoCard = ({ title, value, icon, status }) => {
+  const Icon = Icons[icon];
+
+  const backgroundColor = getStatusColor(status);
+
+  return (
+    <div
+      style={{
+        width: '400px',
+        height: '300px',
+        backgroundColor,
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderRadius: '5px',
+        border: '2px solid #FFFFFF',
+      }}
+    >
+      <div style={{ fontSize: '2rem', marginTop: '1rem' }}>
+        <em>{title}</em>
+      </div>
+      <div style={{
+        width: '300px',
+        height: '100px',
+      }}>
+        {!!Icon && (<Icon style={{ width: '100%', height: '100%' }} />)}
+      </div>
+      <div style={{ fontWeight: 'bold', fontSize: '2rem', marginBottom: '1rem' }}>
+        {value}
+      </div>
+    </div>
+  )
+}
 
 class App extends React.Component {
   constructor (props) {
@@ -36,20 +87,54 @@ class App extends React.Component {
       loading: false,
     });
   }
+
+  renderBody () {
+    if (!this.state.data) {
+      return null;
+    }
+
+    if (this.state.data.type === 'chart') {
+      return (
+        <div
+          style={{
+            width: '400px',
+            height: '300px',
+          }}
+        >
+          <Chart data={chartData} axes={axes} tooltip />
+        </div>
+      )
+    }
+
+    if (this.state.data.type === 'info') {
+      const data = Object.entries(infoData.summary);
+      const rows = chunk(data, 3);
+
+      return (
+        <div style={{ width: '100%', display: 'flex' }}>
+          <div style={{ margin: '0 auto' }}>
+            {rows.map(row => (
+              <div style={{ display: 'flex' }}>
+                {row.map(([title, entry]) => (
+                  <InfoCard
+                    title={title}
+                    value={entry.value}
+                    icon={entry.icon}
+                    status={entry.status}
+                  />
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      )
+    }
+  }
   
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <div
-            style={{
-              width: '400px',
-              height: '300px'
-            }}
-          >
-          <Chart data={data} axes={axes} />
-          </div>
-        </header>
+      <div className="App" style={{ color: '#FFFFFF' }}>
+          {this.renderBody()}
       </div>
     );
   }
---
+++ src/data/index.js
@@ -0,0 +1,62 @@
+export default {
+  "latestRelease": "10.6.60",
+  "updatedAt": "2020-05-06T06:33:08.380Z",
+  "summary": {
+    "Issues Older than 1 week": {
+      "value": 23,
+      "status": "bad",
+      "icon": "FaRegCalendarTimes"
+    },
+    "Average issue age": {
+      "value": "86 days old",
+      "status": "bad",
+      "icon": "FaRegClock"
+    },
+    "Oldest issue": {
+      "value": "345 days old",
+      "status": "bad",
+      "icon": "FaBusinessTime"
+    },
+    "Issues on latest release": {
+      "value": 0,
+      "status": "good",
+      "icon": "FaRegListAlt"
+    },
+    "Latest release version": {
+      "value": "10.6.60",
+      "icon": "FaTabletAlt",
+      "status": "neutral"
+    },
+    "Issues new to latest release": {
+      "value": 0,
+      "status": "good",
+      "icon": "FaExclamationTriangle"
+    },
+    "Unticketed issues": {
+      "value": 9,
+      "status": "bad",
+      "icon": "FaTicketAlt"
+    },
+    "Total issues": {
+      "value": 34,
+      "status": "bad",
+      "icon": "FaCalculator"
+    },
+    "New issues (last 24 hours)": {
+      "value": 2,
+      "status": "okay",
+      "icon": "FaSun"
+    }
+  },
+  "rawUpdate": {
+    "Issues Older than 1 week": 23,
+    "Average issue age": "86 days old",
+    "Oldest issue": "345 days old",
+    "Issues on latest release": 0,
+    "Latest release version": "10.6.60",
+    "Issues new to latest release": 0,
+    "Unticketed issues": 9,
+    "Total issues": 34,
+    "New issues": 2
+  }
+}
